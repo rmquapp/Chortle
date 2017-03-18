f5e03d070854c0aa457813c7a4733e0935abf6dd
Maria Diaz <diazma@ucalgary.ca>
2017-03-17 21:40:40 -0600

    API: Adding the /chores endpoint to get all the chores from parent

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+    console.log(process.env.DATABASE_URL);
   response.render('pages/index');
 });
 
@@ -34,15 +35,34 @@ app.get('/cool', function(request, response) {
 });
 
 app.get('/chores', function(request, response) {
-    var children = [];
+
+    var choresJson = {};
+
     pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-        client.query('SELECT id FROM child WHERE p_id= 1', function (err, resultChildren) {
+        client.query('SELECT ac.id , ac.name chore_name, ac.description, ac.value, ac.status, ch.name ' +
+            'FROM assigned_chore ac LEFT OUTER JOIN child ch ' +
+            'ON (ac.owner = ch.id) WHERE ch.p_id = 1', function (err, result) {
             done();
             if (err) {
                 console.error(err);
             }
             else {
-                response.send("Testing");
+                for (var i = 0; i  < result.rows.length; i++) {
+                    var currentChore = result.rows[i];
+                    if (choresJson[currentChore["name"]] == undefined) {
+                        choresJson[currentChore["name"]] = [];
+                    }
+                    choresJson[currentChore["name"]].push(
+                        {
+                            "id": currentChore["id"],
+                            "name": currentChore["chore_name"],
+                            "description": currentChore["description"],
+                            "value": currentChore["value"],
+                            "status": currentChore["status"],
+                        });
+
+                }
+                response.send(choresJson);
             }
         })
     });
