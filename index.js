0e1713932cc43f453212fc72e782a5307024af69
Maria Diaz <diazma@ucalgary.ca>
2017-03-17 19:27:08 -0600

    Removing function of the get endpoint

--- index.js
+++ index.js
@@ -33,37 +33,37 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
-app.get('/assigned_chore', function (request, response) {
-    let children = [];
-    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-        client.query('SELECT id FROM child WHERE p_id = 1', function (err, resultChildren) {
-            done();
-            if (err) {
-                console.error(err);
-                response.send("Error retrieving child " + err);
-            }
-            else {
-                for (var i = 0; i < resultChildren; i++) {
-                    children.push(resultChildren[i]);
-                }
-
-                if (children.length != 0) {
-                    client.query('SELECT * FROM assigned_chore WHERE owner in (' + children.join(',') + ')', function (errChores, resultChores) {
-                        done();
-                        if (errChores) {
-                            console.error(err);
-                            response.send("Error " + errChores);
-                        }
-                        else {
-                            response.send(resultChores);
-                        }
-
-                    });
-                }
-            }
-        });
-    });
-});
+// app.get('/assigned_chore', function (request, response) {
+//     var children = [];
+//     pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+//         client.query('SELECT id FROM child WHERE p_id = 1', function (err, resultChildren) {
+//             done();
+//             if (err) {
+//                 console.error(err);
+//                 response.send("Error retrieving child " + err);
+//             }
+//             else {
+//                 for (var i = 0; i < resultChildren; i++) {
+//                     children.push(resultChildren[i]);
+//                 }
+//
+//                 if (children.length != 0) {
+//                     client.query('SELECT * FROM assigned_chore WHERE owner in (' + children.join(',') + ')', function (errChores, resultChores) {
+//                         done();
+//                         if (errChores) {
+//                             console.error(err);
+//                             response.send("Error " + errChores);
+//                         }
+//                         else {
+//                             response.send(resultChores);
+//                         }
+//
+//                     });
+//                 }
+//             }
+//         });
+//     });
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
