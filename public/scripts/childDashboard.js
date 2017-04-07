45038803a5c4e5862ab50203ff44cf1e712a92f4
James Pihooja <jlpihooj@ucalgary.ca>
2017-04-06 19:17:50 -0600

    Make Chores Completed

--- public/scripts/childDashboard.js
+++ public/scripts/childDashboard.js
@@ -14,94 +14,78 @@
             funds: 0
         };
 
-        $http.get('child/assigned_chore').
-        success(function(response) {
-            let assigned_chores = response["assigned_chores"];
-            for (let i = 0; i < assigned_chores.length; i++) {
-                let chore = {
-                    id:assigned_chores[i]["id"],
-                    name:assigned_chores[i]["name"],
-                    description:assigned_chores[i]["description"],
-                    value:assigned_chores[i]["value"],
-                    status:assigned_chores[i]["status"]
-                };
-                if(assigned_chores[i]["status"] === "completed"){
-                    $scope.models.pendingApprovalList.push(chore);
-                } else {
-                    $scope.models.choreList.push(chore);
-                }
+        $scope.updateChecked = function(checked, chore) {
+            let status = 'completed';
+            if(checked) {
+                status = 'completed';
+            } else {
+                status = 'assigned';
             }
-            if($scope.models.selected == null){
-                if($scope.models.choreList.length > 0) {
-                    $scope.models.selected = $scope.models.choreList[0];
-                } else if($scope.models.pendingApprovalList.length > 0){
-                    $scope.models.selected = $scope.models.pendingApprovalList[0];
+                let data = {
+                    id : chore.id,
+                    name : chore.name,
+                    description : chore.description,
+                    value : chore.value,
+
+                    status: status
                 }
-            }
-        }).
-        error(function(error) {
-            console.log("Error accessing child assigned chores endpoint");
-        });
-
-        $http.get('/child/funds').
-        success(function(response) {
-            $scope.models.funds = response["funds"];
-            console.log(response);
-        }).
-        error(function(error) {
-            console.log("Error accessing child funds endpoint");
-        });
-
-        $scope.$watch(
-            function combinedWatch() {
-                return {
-                    models: {
-                        selected: $scope.models.selected,
-                        choreList: $scope.models.choreList,
-                        pendingApprovalList: $scope.models.pendingApprovalList,
-                        funds: $scope.models.funds
-                    }
-            };
-        }, function (value) {
-            if (!value.models.choreList) {
-                $scope.models.choreList = [];
-                let promise = service.getHistoricalDataWithQ();
-                promise.then(function (data) {
-                    $scope.models.choreList = data.choreList;
+            $http.put('/child/assigned_chore',data)
+                .success(function (data) {
+                    $scope.loadData();
+                })
+                .error(function (error) {
+                    console.log("Error updating child assigned_chore endpoint");
                 });
-            }
-            if (!value.models.pendingApprovalList) {
+        };
+
+        $scope.loadData = function () {
+            $http.get('child/assigned_chore').
+            success(function(response) {
+                let assigned_chores = response["assigned_chores"];
                 $scope.models.pendingApprovalList = [];
-                let promise = service.getHistoricalDataWithQ();
-                promise.then(function (data) {
-                    $scope.models.pendingApprovalList = data.pendingApprovalList;
-                });
-            }
-            if (!value.models.funds) {
-                $scope.models.funds = 0;
-                let promise = service.getHistoricalDataWithQ();
-                promise.then(function (data) {
-                    $scope.models.funds = data.funds;
-                });
-            }
-        }, true);
+                $scope.models.choreList = [];
+                for (let i = 0; i < assigned_chores.length; i++) {
+                    let chore = {
+                        id:assigned_chores[i]["id"],
+                        name:assigned_chores[i]["name"],
+                        description:assigned_chores[i]["description"],
+                        value:assigned_chores[i]["value"],
+                        status:assigned_chores[i]["status"]
+                    };
+                    if(assigned_chores[i]["status"] === "completed"){
+                        chore.checked = true;
+                        $scope.models.pendingApprovalList.push(chore);
+                    } else {
+                        $scope.models.choreList.push(chore);
+                    }
+                    // ensures the same chore is selected on a re-fetch
+                    if($scope.models.selected != null && $scope.models.selected.id == chore.id){
+                        $scope.models.selected = chore;
+                    }
+                }
+                if($scope.models.selected == null){
+                    if($scope.models.choreList.length > 0) {
+                        $scope.models.selected = $scope.models.choreList[0];
+                    } else if($scope.models.pendingApprovalList.length > 0){
+                        $scope.models.selected = $scope.models.pendingApprovalList[0];
+                    }
+                }
+            }).
+            error(function(error) {
+                console.log("Error accessing child assigned chores endpoint");
+            });
+
+            $http.get('/child/funds').
+            success(function(response) {
+                $scope.models.funds = response["funds"];
+            }).
+            error(function(error) {
+                console.log("Error accessing child funds endpoint");
+            });
+        };
+
+        //initial load
+        $scope.loadData();
     });
-// http://stackoverflow.com/questions/14154767/angular-js-refresh-service-inside-a-controller
-    app.factory('service', function ($q, $http) {
-        return {
-            // return a promise to controller
-            getHistoricalDataWithQ: function () {
-                let deferred = $q.defer();
-                let url = 'https://chortle-seng513.herokuapp.com/chores';
-                $http.get(url)
-                    .success(function (data) {
-                        deferred.resolve(data);
-                    })
-                    .error(function (error) {
-                        console.log(JSON.stringify(error));
-                    });
-                return deferred.promise;
-            }
-        }
-    });
+
 })(window.angular);
